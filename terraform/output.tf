5a82ee6e78aad3491551cec5456eb2763239c4c1
Ringo De Smet <ringo@de-smet.name>
2017-10-14 15:21:18 +0200

    Migration from Cloudfront/S3 to Netlify.

--- terraform/output.tf
+++
@@ -1,7 +0,0 @@
-output "deployer_access_key" {
-  value = "${aws_iam_access_key.ringo_de_smet_name_deployer_key.id}"
-}
-
-output "deployer_secret_key" {
-  value = "${aws_iam_access_key.ringo_de_smet_name_deployer_key.secret}"
-}
